ca62d4bbfdf5474e4a6634b4ba02f9ef5ec4b524
rubens-lavor <haryson07@gmail.com>
2020-08-29 01:08:04 -0300

    exibindo string na tela e conhecendo o jsx

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const tag = <strong>Olá React!!!</strong>
+
+ReactDOM.render(
+    <div>
+        { tag }
+    </div>,
+    document.getElementById("root")
+);
